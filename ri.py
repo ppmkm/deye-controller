f5086d1348a240f20f7e41978ffd4b1b065d5d3e
sp9muf <piotr@pi>
2025-10-08 00:34:55 +0200

    working on manipulaton of registers

--- ri.py
+++ ri.py
@@ -1,2 +1,3 @@
+#!/usr/local/bin/python3
 from deye_controller.deye_reader import read_inverter
 read_inverter('10.96.0.23', 2781383066, as_json=False, to_file=None)
---
+++ set_grid_charge.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+from deye_controller.sell_programmer import SellProgrammer
+
+prog = SellProgrammer('10.96.0.23', 2781383066)
+prog.show_as_screen()
+                                
